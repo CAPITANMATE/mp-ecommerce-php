4e147fd4bc25ffa7295ae855cc6055b07acab306
Damián Appugliese <appugliesedamian@email.com>
2020-09-03 12:09:58 -0300

    PAGAR 3

--- process_payment.php
+++ process_payment.php
@@ -1,6 +1,16 @@
  <?php
     require_once 'vendor/autoload.php';
 
+
+echo $_POST['transactionAmount'] . "<BR>"; 
+echo $_POST['token'] . "<BR>"; 
+echo $_POST['description'] . "<BR>"; 
+echo $_POST['installments'] . "<BR>"; 
+echo $_POST['paymentMethodId'] . "<BR>"; 
+echo $_POST['issuer'] . "<BR>"; 
+echo $_POST['email'] . "<BR>"; 
+echo $_POST['docType'] . "<BR>"; 
+echo $_POST['docNumber'] . "<BR>"; 
  
 	MercadoPago\SDK::setAccessToken("TEST-4162999866747892-090313-50dc05b70bfeccafb2de5278f69c6c9d-325440040");
     $payment = new MercadoPago\Payment();
@@ -12,7 +22,7 @@
     $payment->issuer_id = (int)$_POST['issuer'];
 
 
- echo json_encode($payment) "<br>";
+ 
 
     $payer = new MercadoPago\Payer();
     $payer->email = $_POST['email'];
